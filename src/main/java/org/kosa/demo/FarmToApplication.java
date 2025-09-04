0147c4b277948a0e197c7d6173ab9c72a34994ec
minho2618 <minho2618@naver.com>
2025-09-04 14:16:34 +0900

    Project Init

---
+++ src/main/java/org/kosa/demo/FarmToApplication.java
@@ -0,0 +1,13 @@
+package org.kosa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmToApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmToApplication.class, args);
+	}
+
+}
