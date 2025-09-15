026a5ae82fb33cf76ced8b946f178303caff3f36
sunhokim1 <92263333+sunhokim1@users.noreply.github.com>
2025-09-15 16:31:05 +0900

    add : Exception, advice

---
+++ src/main/java/org/kosa/service/MemberService.java
@@ -0,0 +1,25 @@
+package org.kosa.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.kosa.dto.member.SignUpReq;
+import org.kosa.entity.Member;
+import org.kosa.repository.MemberRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Transactional
+    public void signUp(SignUpReq signUpReq){
+        Member cMember = signUpReq.toMember();
+        String pwdEnc = bCryptPasswordEncoder.encode(signUpReq.getPassword());
+        cMember.setPassword(pwdEnc);
+        memberRepository.save(cMember);
+    }
+
+}
