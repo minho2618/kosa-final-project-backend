1102763c3b684202fbbce91d62112f199fc34f22
sunhokim1 <92263333+sunhokim1@users.noreply.github.com>
2025-09-15 17:09:44 +0900

    fix : RecordNotFoundException

--- src/main/java/org/kosa/service/MemberService.java
+++ src/main/java/org/kosa/service/MemberService.java
@@ -4,9 +4,14 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.kosa.dto.member.SignUpReq;
 import org.kosa.entity.Member;
+import org.kosa.exception.DuplicateException;
+import org.kosa.exception.InvalidInputException;
+import org.kosa.exception.RecordNotFoundException;
 import org.kosa.repository.MemberRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,11 +20,10 @@ public class MemberService {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Transactional
-    public void signUp(SignUpReq signUpReq){
+    public void signUp(SignUpReq signUpReq) throws DuplicateException, InvalidInputException {
         Member cMember = signUpReq.toMember();
         String pwdEnc = bCryptPasswordEncoder.encode(signUpReq.getPassword());
         cMember.setPassword(pwdEnc);
         memberRepository.save(cMember);
     }
-
 }
